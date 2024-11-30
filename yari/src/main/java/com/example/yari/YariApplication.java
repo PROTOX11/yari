dc93eeb622472dce9117f93d07039afd5c6e20b8
Prakash kumar <93497675+Rtxneon@users.noreply.github.com>
2024-11-30 12:11:53 +0530

    Initial commit

---
+++ yari/src/main/java/com/example/yari/YariApplication.java
@@ -0,0 +1,13 @@
+package com.example.yari;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YariApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YariApplication.class, args);
+	}
+
+}
