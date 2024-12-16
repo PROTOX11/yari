0789a57880ac8ac61a5b22c48eed339140044d3e
Prakash kumar <93497675+Rtxneon@users.noreply.github.com>
2024-12-17 00:47:51 +0530

    tailwind installed
    tailwind cli

---
+++ yari/src/main/java/com/example/yari/controllers/PageController.java
@@ -0,0 +1,18 @@
+package com.example.yari.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController {
+    @RequestMapping("/home")
+    public String home(Model model) {
+        System.out.println("Home page handler");
+        model.addAttribute("name", "Substring Technologies");
+        model.addAttribute("youtube_channel","drink the code");
+        model.addAttribute("github","https://github.com/PROTOX11");
+
+        return "home";
+    }
+}
